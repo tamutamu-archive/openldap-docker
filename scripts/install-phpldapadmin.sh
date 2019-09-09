5cb670a4bfe6c3313af80fcf2237e407d7b6ce6e
tamutamu <tamu.0.0.tamu@gmail.com>
2019-09-09 11:31:53 +0900

    Update installing phpldapadmin.

--- scripts/install-phpldapadmin.sh
+++ scripts/install-phpldapadmin.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-#apt -y install apache2 php php-cgi libapache2-mod-php php-mbstring php-common php-pear
-apt -y install apache2
+apt -y install apache2 php php-ldap php-cgi libapache2-mod-php php-mbstring php-common php-pear
 
 a2enconf php7.2-cgi
 systemctl reload apache2
 
-apt -y install phpldapadmin
+pushd /tmp
+wget --no-check-certificate https://github.com/leenooks/phpLDAPadmin/archive/1.2.5.tar.gz
+tar xf 1.2.5.tar.gz
+cp -r phpLDAPadmin-1.2.5 /var/www/html/phpldapadmin
+chown www-data /var/www/html/ -R
+cp /var/www/html/phpldapadmin/config/config.php{.example,}
+popd
 
 systemctl restart apache2
